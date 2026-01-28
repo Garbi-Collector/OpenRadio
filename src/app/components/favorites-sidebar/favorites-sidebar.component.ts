ca2eac1f0a527c69894f3eb402387c1ca57e9f45
Garbi-Collector <uwurulos@gmail.com>
2026-01-28 08:58:55 -0300

    Add FavoritesSidebarComponent
    Introduces a new standalone Angular component 'FavoritesSidebarComponent' with initial template and style files.

---
+++ src/app/components/favorites-sidebar/favorites-sidebar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-favorites-sidebar',
+  standalone: true,
+  imports: [],
+  templateUrl: './favorites-sidebar.component.html',
+  styleUrl: './favorites-sidebar.component.css'
+})
+export class FavoritesSidebarComponent {
+
+}
